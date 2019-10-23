634dad82ae5ee3386121994639d5555214d41dff
Erik James <36680116+anothererikjames@users.noreply.github.com>
2019-10-23 11:21:53 -0700

    Delete run-runner.sh

--- run-runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APPIPA=LoanCalc.ipa
-TESTIPA=LoanCalcUITests-Runner.ipa
-DEVICE=iPhone_6S_carrier_test_sjc
-
-#runner.sh config --path runner.yml --apikey ${LOANCALC_IOS_APIKEY} $*
-runner.sh xcuitest --test ${TESTIPA} --app ${APPIPA} --apikey ${LOANCALC_IOS_APIKEY} --datacenter US --device ${DEVICE} --testsToRun LoanCalcUITests/testLoanCalc1,LoanCalcUITests/testLoanCalc2 $*
